0f419e0acf9df97476a5a35e0233eda8ba4e744d
sean <sean@finchain360.com>
2018-03-31 18:57:26 +0800

    实现接口方法

--- bc_bdb/src/main/java/org/brewchain/backend/bc_bdb/provider/OBDBImpl.java
+++ bc_bdb/src/main/java/org/brewchain/backend/bc_bdb/provider/OBDBImpl.java
@@ -1,5 +1,6 @@
 package org.brewchain.backend.bc_bdb.provider;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 
@@ -15,6 +16,8 @@ import com.google.protobuf.ByteString;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.DatabaseEntry;
 import com.sleepycat.je.LockMode;
+import com.sleepycat.je.Transaction;
+import com.sleepycat.je.TransactionConfig;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -69,39 +72,146 @@ public class OBDBImpl implements ODBSupport, DomainDaoSupport {
 	}
 
 	@Override
-	public Future<OValue[]> batchCompareAndDelete(OKey[] arg0, OValue[] arg1) throws ODBException {
+	public Future<OValue[]> batchCompareAndDelete(OKey[] keys, OValue[] values) throws ODBException {
+		Transaction txn = null;
+		List<OValue> list = new ArrayList<OValue>();
+		try {
+			txn = this.dbs.getEnvironment().beginTransaction(null, null);
+			for (int i = 0; i < keys.length; i++) {
+				DatabaseEntry searchEntry = new DatabaseEntry();
+				dbs.get(txn, new DatabaseEntry(keys[i].toByteArray()), searchEntry, LockMode.DEFAULT);
+				if (searchEntry.getData() == null) {
+
+				} else {
+					OValue v = ODBHelper.b2Value(searchEntry.getData());
+					if (v.equals(values[i])) {
+
+					} else {
+						dbs.delete(txn, new DatabaseEntry(keys[i].toByteArray()));
+						// return old value
+						list.add(v);
+					}
+				}
+			}
+			txn.commitSync();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return ConcurrentUtils.constantFuture((OValue[]) list.toArray());
+	}
+
+	@Override
+	public Future<OValue[]> batchCompareAndSwap(OKey[] keys, OValue[] compareValues, OValue[] newValues)
+			throws ODBException {
+		Transaction txn = null;
+		List<OValue> list = new ArrayList<OValue>();
+		try {
+			txn = this.dbs.getEnvironment().beginTransaction(null, null);
+			for (int i = 0; i < keys.length; i++) {
+				DatabaseEntry searchEntry = new DatabaseEntry();
+				dbs.get(txn, new DatabaseEntry(keys[i].toByteArray()), searchEntry, LockMode.DEFAULT);
+				if (searchEntry.getData() == null) {
+
+				} else {
+					OValue v = ODBHelper.b2Value(searchEntry.getData());
+					if (v.equals(compareValues[i])) {
+
+					} else {
+						dbs.put(txn, new DatabaseEntry(keys[i].toByteArray()),
+								new DatabaseEntry(newValues[i].toByteArray()));
+						// return old value
+						list.add(v);
+					}
+				}
+			}
+			txn.commitSync();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return ConcurrentUtils.constantFuture((OValue[]) list.toArray());
+	}
+
+	@Override
+	public Future<OValue[]> batchDelete(OKey[] keys) throws ODBException {
+		Transaction txn = null;
+		try {
+			// need TransactionConfig?
+			txn = this.dbs.getEnvironment().beginTransaction(null, null);
+			for (OKey key : keys) {
+				this.dbs.delete(txn, new DatabaseEntry(key.toByteArray()));
+			}
+			txn.commitSync();
+		} catch (Exception ex) {
+			txn.abort();
+			log.error("fail to batch delete::ex=" + ex);
+		}
+
 		return ConcurrentUtils.constantFuture(null);
 	}
 
 	@Override
-	public Future<OValue[]> batchCompareAndSwap(OKey[] arg0, OValue[] arg1, OValue[] arg2) throws ODBException {
+	public Future<OValue[]> batchPuts(OKey[] keys, OValue[] values) throws ODBException {
+		Transaction txn = null;
+		try {
+			// need TransactionConfig?
+			txn = this.dbs.getEnvironment().beginTransaction(null, null);
+			for (int i = 0; i < keys.length; i++) {
+				this.dbs.put(txn, new DatabaseEntry(keys[i].toByteArray()),
+						new DatabaseEntry(ODBHelper.v2Bytes(values[i])));
+			}
+			txn.commitSync();
+		} catch (Exception ex) {
+			txn.abort();
+			log.error("fail to batch put::ex=" + ex);
+
+		}
 		return ConcurrentUtils.constantFuture(null);
 	}
 
 	@Override
-	public Future<OValue[]> batchDelete(OKey[] arg0) throws ODBException {
-		return ConcurrentUtils.constantFuture(null);
-	}
-
-	@Override
-	public Future<OValue[]> batchPuts(OKey[] arg0, OValue[] arg1) throws ODBException {
-		return ConcurrentUtils.constantFuture(null);
-	}
-
-	@Override
-	public Future<OValue> compareAndDelete(OKey arg0, OValue arg1) throws ODBException {
-		return ConcurrentUtils.constantFuture(null);
-	}
-
-	@Override
-	public Future<OValue> compareAndSwap(OKey arg0, OValue arg1, OValue arg2) throws ODBException {
-		return ConcurrentUtils.constantFuture(null);
-	}
-
-	@Override
-	public Future<OValue> delete(OKey arg0) throws ODBException {
+	public Future<OValue> compareAndDelete(OKey key, OValue value) throws ODBException {
+		DatabaseEntry searchEntry = new DatabaseEntry();
+		dbs.get(null, new DatabaseEntry(key.toByteArray()), searchEntry, LockMode.DEFAULT);
+		if (searchEntry.getData() == null) {
 			return ConcurrentUtils.constantFuture(null);
+		} else {
+			OValue v = ODBHelper.b2Value(searchEntry.getData());
+			if (v.equals(value)) {
 
+			} else {
+				dbs.delete(null, new DatabaseEntry(key.toByteArray()));
+				trySync();
+			}
+
+			// return new value or old value?
+			return ConcurrentUtils.constantFuture(v);
+		}
+	}
+
+	@Override
+	public Future<OValue> compareAndSwap(OKey key, OValue compareValue, OValue newValue) throws ODBException {
+		DatabaseEntry searchEntry = new DatabaseEntry();
+		dbs.get(null, new DatabaseEntry(key.toByteArray()), searchEntry, LockMode.DEFAULT);
+		if (searchEntry.getData() == null) {
+			return ConcurrentUtils.constantFuture(null);
+		} else {
+			OValue v = ODBHelper.b2Value(searchEntry.getData());
+			if (v.equals(compareValue)) {
+
+			} else {
+				dbs.put(null, new DatabaseEntry(key.toByteArray()), new DatabaseEntry(ODBHelper.v2Bytes(newValue)));
+				trySync();
+			}
+
+			return ConcurrentUtils.constantFuture(newValue);
+		}
+	}
+
+	@Override
+	public Future<OValue> delete(OKey key) throws ODBException {
+		dbs.delete(null, new DatabaseEntry(key.toByteArray()));
+		trySync();
+		return ConcurrentUtils.constantFuture(null);
 	}
 
 	@Override
@@ -117,8 +227,18 @@ public class OBDBImpl implements ODBSupport, DomainDaoSupport {
 	}
 
 	@Override
-	public Future<OValue[]> list(OKey[] arg0) throws ODBException {
-		return ConcurrentUtils.constantFuture(null);
+	public Future<OValue[]> list(OKey[] keys) throws ODBException {
+		List<OValue> list = new ArrayList<OValue>();
+		for (OKey key : keys) {
+			DatabaseEntry searchEntry = new DatabaseEntry();
+			dbs.get(null, new DatabaseEntry(key.toByteArray()), searchEntry, LockMode.DEFAULT);
+			if (searchEntry.getData() == null) {
+
+			} else {
+				list.add(ODBHelper.b2Value(searchEntry.getData()));
+			}
+		}
+		return ConcurrentUtils.constantFuture((OValue[]) list.toArray());
 	}
 
 	@Override
@@ -151,8 +271,7 @@ public class OBDBImpl implements ODBSupport, DomainDaoSupport {
 
 	@Override
 	public Future<OValue> put(String key, OValue value) throws ODBException {
-		return put(OKey.newBuilder().setData(ByteString.copyFrom(key.getBytes())).build()
-				,value);
+		return put(OKey.newBuilder().setData(ByteString.copyFrom(key.getBytes())).build(), value);
 
 	}
 
